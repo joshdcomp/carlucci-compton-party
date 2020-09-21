c4d06e6cf0f72207053fa320b291092d673aa708
Josh Compton <jcompton@learnvest.com>
2020-09-20 23:38:03 -0400

    feat: adds new icons, refactors icons to be pulled from one source

--- src/components/icons/home.js
+++ src/components/icons/home.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-const IconHome = () => (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
+export const Home = () => (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
 </svg>)
-export default IconHome
--- src/components/icons/menu.js
+++ src/components/icons/menu.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-const IconMenu = () => (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+export const Menu = () => (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h8m-8 6h16" />
 </svg>)
-
-export default IconMenu
