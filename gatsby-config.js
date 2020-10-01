ee608f391ba04c69ef26efca9f26c9db274c12fa
Josh Compton <jcompton@learnvest.com>
2020-09-30 22:55:39 -0400

    fix: favicon location

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `assets/favicon.png`,
+        icon: `static/assets/favicon.png`,
       },
     },
   ],
