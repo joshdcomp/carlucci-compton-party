0bf6f875bae2913fc34880e1e2c796ad16d9c79a
Josh Compton <jcompton@learnvest.com>
2021-04-02 11:23:12 -0400

    fix: updates date for header info

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,20 +5,18 @@ import SEO from "../components/seo"
 import IntroText from "../components/intro-text"
 import IntroTiles from "../components/intro-tiles"
 
-const HomePage = () => {
-  return (
-    <>
-      <SEO
-        title={`Michele & Josh are getting married!`}
-        description={`Join us on May 8, 2021 for our wedding celebration`}
-      />
-      
-      <Layout>
-          <IntroText />
-          <IntroTiles />
-      </Layout>
-    </>
-  )
-}
+const HomePage = () => (
+  <>
+    <SEO
+      title={`Michele & Josh are getting married!`}
+      description={`Join us on May 7, 2022 for our wedding celebration!`}
+    />
+
+    <Layout>
+        <IntroText />
+        <IntroTiles />
+    </Layout>
+  </>
+)
 
 export default HomePage
